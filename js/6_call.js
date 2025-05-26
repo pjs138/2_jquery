e8681f20dcc699e9117b8f296ff69f8923a646ac
pjs138 <junesang1210@gmail.com>
2025-05-26 11:53:29 +0900

    Add files via upload

---
+++ js/6_call.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("button#hideBtn").click(function(){
+        $("p").hide("slow", function(){
+            alert("난 사라짐");
+        })
+    });
+});
+
+// 자바스크립트 
+function myDisplayer(some){
+    document.getElementById("call").innerHTML = some;
+}
+
+function myFirst(){
+    myDisplayer("pjs");
+}
+
+function mySecond(){
+    myDisplayer("out");
+}
+
+// 함수는 호출할때만 실행되는 코드블록
+myFirst();
+mySecond();
---
+++ js/7_chain.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ // 비동기
+    $("button#re").click(function(){
+        $("#p1").css("color","red").slideUp(2000).slideDown(2000)/*.css("fontSize","30rem")*/
+        })
+})
